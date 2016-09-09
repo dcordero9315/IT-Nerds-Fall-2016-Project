4dfff8221caa769588ce97b4b37d078549c5db8c
Dan Cordero <dcorder1@villanova.edu>
2016-09-08 20:29:01 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+# Prompt user to start the game
+# Declare empty array for the word
+
+# Random word generated
+
+word = 'test'
+
+wordList = []
+
+guess = input("What is your guess?:")
+
+
+if (guess == word):
+    print("This has already been guessed")
+
+
+#print(word)
