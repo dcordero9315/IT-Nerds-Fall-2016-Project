a89c4942ca5ee23d3280137f9b8ab35844c7206c
Dan Cordero <dcorder1@villanova.edu>
2016-09-13 19:38:20 -0400

    Added algorithm in comments
    Also added a couple of more lines of code

--- hangman.py
+++ hangman.py
@@ -2,9 +2,52 @@ import random
 import string
 
 # Prompt user to start the game
+
+
 # Declare empty array for the word
 
+
+
 # Random word generated
+
+
+
+# Show the number of letters
+
+
+
+# would you like to guess a word or letter
+
+guessType = input("Would you like to guess a word or letter?:")
+
+# while count is less than or equal to 9
+
+    # if letter
+
+        # check if it's in word and not in word list
+            # print to screen
+
+        # if not display wrong message and part of body
+            # add to counter
+
+        # if letter has been used
+            # print error
+            # add to counter
+
+    # if word
+
+        # check if it matches the word
+            # Show win screen
+
+        # if does not match word OR has been used before
+            # display message and show next body part
+            # add to counter
+
+# check counter
+
+
+
+
 
 word = 'test'
 
@@ -17,4 +60,3 @@ if (guess == word):
     print("This has already been guessed")
 
 
-#print(word)
