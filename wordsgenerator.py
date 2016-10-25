3fe94d7f1d485497f5f1b3113e730e6752da4097
annagk <annagk@gmail.com>
2016-10-25 08:32:11 -0400

    Add files via upload

--- wordsgenerator.py
+++ wordsgenerator.py
@@ -11,20 +11,22 @@ index = 0
 finished = False
 guesses = 0
 letters_used = " "
+words_used = " "
 found = False
 
 
-
-print (word)
-print(len(word))
+print('Lets play Hangman!!')
+print ('')
+#print (word)
+#print(len(word))
 while not finished:
         if len(word) < 5 or len(word) > 10:
                 word = rw.random_word()
-                print(word)
+                #print(word)
         else:
                 finished=True
 
-print(len(word))
+#print(len(word))
 
 space = len(word)
 underscore = ("_ " * space)
@@ -38,12 +40,14 @@ correct = 0
 
 for i in range(1, 9):
     index = 0
-    var = raw_input("What's your guess? ")
+    var = input("\nWhat's your guess? ")
+
 
     if var == word:
         print("correct")
         break
     else:
+
 
         while index < len(word):
   #  for num in range (0,10):
@@ -51,22 +55,27 @@ for i in range(1, 9):
 
             if index == -1:
                 if found == False:
-                    print (index)
-                    print ('Incorrect!')
-                    print (var +  '  - does not exist in this word. ')
-                    letters_used =  letters_used + ' ' + var
-                    print ('letter used: [' + letters_used + ']')
+                    print ('\nIncorrect!')
+                    print ("\n" + var +  '  - does not exist in this word. ')
+                    if len(var) > 1:
+                        words_used =  words_used + ' ' + var
+                        print ('\nwords used: [' + words_used + ']')
+                   # else:
+                letters_used =  letters_used + ' ' + var
+                print ('\nletter used: [' + letters_used + ']')
+                    #print ('\nletter used: [' + letters_used + ']')
                 break
             else:
                 found = True
-                print(var + ' found at', index)
+                print("\n" + var + ' found at', index)
                 index += 1
                 guessed[var] = 1
                 correct += 1
+                print ("\n")
                 print(" ".join([ch if guessed[ch] else "_" for ch in word]))
         found = False
 else:
-    print("You lose!\nThe word was {}".format(word))
+    print("\nYou lose!\nThe word was {}".format(word))
 #underscore = ("_ " * space)
 # print(underscore)
 
